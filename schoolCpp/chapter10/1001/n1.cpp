787ee3ef4f45fd887a3fbbeb11be542689426085
abcd1234davidchen <abcd1234davidchen@gmail.com>
2024-05-22 19:27:02 +0800

    rewrite

--- schoolCpp/chapter10/1001/n1.cpp
+++ schoolCpp/chapter10/1001/n1.cpp
@@ -1,87 +1,97 @@
 #include<iostream>
 using namespace std;
 
-class TwoD {
-private:
-    double* data;
-    int MaxRows;
-    int MaxCols;
-
-public:
-    // Default constructor
-    TwoD(int mRows,int mCols):MaxRows(mRows),MaxCols(mCols){
-        data=new double[MaxRows*MaxCols]();
-    }
-    // Copy constructor
-    TwoD(const TwoD& other){
-        MaxRows=other.MaxRows;
-        MaxCols=other.MaxCols;
-        data=new double[MaxRows*MaxCols]();
-        copy(other.data,other.data+MaxRows*MaxCols,data);
-    }
-
-    // Destructor
-    ~TwoD(){
-        delete[] data;
-    }
-
-    // Overloaded assignment operator
-    TwoD& operator=(const TwoD& other){
-        if(this!=&other){
-            delete[] data;
-            MaxRows=other.MaxRows;
-            MaxCols=other.MaxCols;
-            data=new double[MaxRows*MaxCols];
-            copy(other.data,other.data+MaxRows*MaxCols,data);
-        }
-        return *this;
-    }
-    // Accessor function
-    double get(int row,int col) const {
-        return data[row*MaxCols+col];
-    }
-    // Mutator function
-    void set(int row,int col,double value){
-        data[row*MaxCols+col]=value;
-    }
-    // Overloaded + operator
-    friend TwoD operator+(const TwoD& one,const TwoD& two){
-        int mRow=(one.MaxRows>two.MaxRows)?one.MaxRows:two.MaxRows;
-        int mCol=(one.MaxCols>two.MaxCols)?one.MaxCols:two.MaxCols;
-        TwoD result(mRow,mCol);
-        for (int i = 0; i < one.MaxRows; i++) {
-            for (int j = 0; j < one.MaxCols; j++) {
-                result.set(i, j, one.get(i,j));
-            }
-        }
-
-        for (int i = 0; i < two.MaxRows; i++) {
-            for (int j = 0; j < two.MaxCols; j++) {
-                result.set(i, j, result.get(i,j) + two.get(i,j));
-            }
-        }
-        return result;
-    }
-    friend ostream& operator<<(ostream& out,const TwoD& one){
-        for(int i=0;i<2;i++){
-            for(int j=0;j<2;j++){
-                out<<one.get(i,j);
-            }
-            out<<endl;
-        }
-        return out;
-    }
+typedef double* dptr;
+class TwoD{
+	public:
+		TwoD():MaxRows(2),MaxCols(2){
+			a= new double[2*2]();
+		}
+		TwoD (int rows, int cols):MaxRows(rows),MaxCols(cols){
+			a= new double[rows*cols]();
+		}
+		TwoD(const TwoD& array0){
+			MaxRows=array0.MaxRows;
+			MaxCols=array0.MaxCols;
+			a=new double[MaxRows*MaxCols]();
+			copy(array0.a,array0.a+MaxRows*MaxCols,a);
+		}
+		void setValue(int i, int j, double value){
+			a[i*MaxRows+j]=value;
+		}
+		double getValue(int rows, int cols){
+			return a[rows*MaxRows+cols];
+		}
+		friend const TwoD operator+(const TwoD& arrayA, const TwoD& arrayB){
+			TwoD temp(arrayA);
+			for(int i=0;i<temp.MaxRows;i++){
+				for(int j=0;j<temp.MaxCols;j++){
+					temp.setValue(i,j,arrayA.a[i*temp.MaxRows+j]+arrayB.a[i*temp.MaxRows+j]);
+				}
+			}
+			return temp;
+		}
+		TwoD& operator=(const TwoD& rvalue){
+			if(this!=&rvalue){
+				delete[] a;
+				MaxRows=rvalue.MaxRows;
+				MaxCols=rvalue.MaxCols;
+				a=new double[rvalue.MaxRows*rvalue.MaxCols];
+				copy(rvalue.a,rvalue.a+MaxRows*MaxCols,a);
+			}
+			return *this;		
+		}
+		
+		~TwoD(){
+			delete[] a;
+		}
+	private:
+		int MaxRows, MaxCols;
+		dptr a;
 };
+
 int main(){
-    srand(time(0));
-    TwoD one(2,2);
-    for(int i=0;i<2;i++){
-        for(int j=0;j<2;j++){
-            one.set(i,j,rand()%5);
-        }
-    }
-    cout<<one;
-    TwoD two(one);
-    cout<<two;
-    cout<<one+two;    
+	int row, col;
+	double number;
+	cout<<"Enter the number of rows: ";
+	cin>>row;
+	cout<<"\nEnter the number of columns: ";
+	cin>>col;
+	cout<< "\nInsert the values in first array\n";
+	
+	TwoD array1(row, col);
+	for (int i=0;i<row;i++){
+		for(int j=0;j<col;j++){
+			cout<<"enter the value in the"<<i<<"th row of "<<j<<"th column\n";
+			cin>>number;
+			array1.setValue(i,j, number);
+		}
+	}
+	cout<<"the value in an two dimensional array "<<endl;
+	for(int i = 0;i<row;i++){
+		for(int j=0;j<col;j++){
+			cout<<array1.getValue(i,j)<<" ";
+		}
+		cout<<endl;
+	}
+	cout << "Insert values in second array"<<endl;
+	TwoD array2(row, col);
+	for(int i=0;i<row;i++){
+		for(int j= 0;j<col;j++){
+			cout << "Enter the value in the "<<i<<"th row of " << j<<"th column\n";
+			cin>>number;
+			array2.setValue(i,j,number);
+		}
+	}
+	cout<<"Addition of two arrays\n";
+	TwoD array3(row, col);
+	array3 = array1 + array2;
+	cout << "The Result is: \n";
+	for(int i=0;i<row;i++){
+		for(int j= 0;j<col;j++){
+			cout << array3.getValue(i,j) << " ";
+		}
+		cout<<endl;
+	}
+	return 0;
 }
