b4f984c968a9d16529f16ee197c82822d292695c
abcd1234davidchen <abcd1234davidchen@gmail.com>
2024-08-28 03:00:20 +0800

    try out some random test on threads

---
+++ schoolCpp/test/randomThreadTest/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<81;i++){
+        cout<<i/9+1<<'*'<<i%9+1<<'='<<(i%9+1)*(i/9+1)<<' ';
+        if(i%3==2){
+            cout<<endl;
+        }
+    }
+
+    int a=4,b=5;
+    a=a^b;
+    b=a^b;
+    a=a^b;
+    cout<<a<<b;
+}
