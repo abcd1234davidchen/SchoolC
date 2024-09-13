6e499867922964f0f50856f122554403cabf59f2
abcd1234davidchen <abcd1234davidchen@gmail.com>
2024-09-13 21:52:57 +0800

    seperate function out from main

--- dataStructure/hw1.2/1b.cpp
+++ dataStructure/hw1.2/1b.cpp
@@ -2,14 +2,14 @@
 #include <vector>
 using namespace std;
 
-int main(){
+void fact(int n){
     vector<int> vec;
     vec.push_back(1);
-    for(int i=1;i<=50;i++){
-        for(int j=0;j<vec.size();j++){
+    for(int i=1;i<=n;i++){
+        for(size_t j=0;j<vec.size();j++){
             vec[j]*=i;
         }
-        for (int j = 0; j < vec.size(); j++) {
+        for (size_t j=0;j<vec.size();j++) {
             if (vec[j] >= 10) {
                 if (vec.size()==j+1) {
                     vec.push_back(0);
@@ -24,4 +24,8 @@ int main(){
         }
         cout<<endl<<endl;
     }
+}
+
+int main(){
+    fact(6);
 }
