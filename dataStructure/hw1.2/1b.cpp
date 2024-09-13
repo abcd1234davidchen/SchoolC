96ce03ceecf0e5fba184191a732bfadec72c6e07
abcd1234davidchen <abcd1234davidchen@gmail.com>
2024-09-13 19:32:40 +0800

    hw2

--- dataStructure/hw1.2/1b.cpp
+++ dataStructure/hw1.2/1b.cpp
@@ -5,14 +5,18 @@ using namespace std;
 int main(){
     vector<int> vec;
     vec.push_back(1);
-    for(int i=1;i<10;i++){
+    for(int i=1;i<=50;i++){
         for(int j=0;j<vec.size();j++){
             vec[j]*=i;
         }
-        for(int j=0;j<vec.size();j++){
-            if(vec.size()>(j+1)) vec.push_back(0);
-            vec[j+1]+=vec[j]/10;
-            vec[j]%=10;
+        for (int j = 0; j < vec.size(); j++) {
+            if (vec[j] >= 10) {
+                if (vec.size()==j+1) {
+                    vec.push_back(0);
+                }
+                vec[j+1] += vec[j] / 10;
+                vec[j] %= 10;
+            }
         }
         cout<<"VEC"<<i<<endl;
         for(int j=vec.size()-1;j>=0;j--){
