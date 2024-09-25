77352565b3c3656e058cf21c263f9ee75247e836
abcd1234davidchen <abcd1234davidchen@gmail.com>
2024-09-25 13:57:57 +0800

    n11-12 cpe

--- dataStructure/oneStar/n11/n11.cpp
+++ dataStructure/oneStar/n11/n11.cpp
@@ -3,22 +3,22 @@ using namespace std;
 
 int main(){
     string ipt;
-    int flat=0;
-    cin>>ipt;
-
-    for(size_t i=0;i<ipt.size();i++){
-        flat+=(ipt[i]-'0');
-    }
-    cout<<"FLAT"<<flat<<endl;
-    while(flat>=10){
-        int newFlat = 0;
-        int processFlat = flat;
-        while(processFlat>=10){
-            newFlat+=processFlat%10;
-            processFlat/=10;
+    while(cin>>ipt){
+        int flat=0;
+            for(size_t i=0;i<ipt.size();i++){
+            flat+=(ipt[i]-'0');
         }
-        newFlat+=processFlat%10;
-        flat = newFlat;
+        //cout<<"FLAT"<<flat<<endl;
+        while(flat>=10){
+            int newFlat = 0;
+            int processFlat = flat;
+            while(processFlat>=10){
+                newFlat+=processFlat%10;
+                processFlat/=10;
+            }
+            newFlat+=processFlat%10;
+            flat = newFlat;
+        }
+        cout<<flat<<endl;
     }
-    cout<<flat;
 }
